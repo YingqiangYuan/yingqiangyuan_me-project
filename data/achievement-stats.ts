05cd867edf998f2d0ecf2842866eb8633da6bfc3
YingQiang Yuan <yingqiang.yuan@gmail.com>
2026-05-06 20:05:18 -0400

    Create achievement-stats.ts

---
+++ data/achievement-stats.ts
@@ -0,0 +1,15 @@
+import { FaPython } from "react-icons/fa"
+import { PenTool, Sparkles } from "lucide-react"
+import { AchievementStat } from "@/types"
+
+export const achievementStats: AchievementStat[] = [
+  {
+    number: "...",
+    description: "...",
+    color: "text-primary",
+    borderColor: "border-primary",
+    glowColor: "shadow-primary/20",
+    icon: FaPython,
+    href: "...",
+  },
+]
